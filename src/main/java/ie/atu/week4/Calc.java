0bcbc14e6708a74ab312fd7fe4367c83e7cdc08a
g00391231 <g00391231@atu.ie>
2022-10-12 12:29:29 +0100

    Added multiply method to class Calc

--- src/main/java/ie/atu/week4/Calc.java
+++ src/main/java/ie/atu/week4/Calc.java
@@ -1,7 +1,13 @@
 package ie.atu.week4;
 
 public class Calc {
+    public int multiply(int x,int y)
+    {
+        int z = x * y;
+        return z;
+    }
     public static void main(String[] args) {
+
 
     }
 }
